e236f3441a3ede7c54d7d6ade1ee43bc3ea4b0dd
gaiyadev <gaiyaobed94@gmail.com>
2021-08-19 00:50:13 +0100

    bug

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  let page = express.static("/client/next/server/pages/index.html");
-
-  res.render(page, { title: "Express" });
+  res.render('index', { title: "Express" });
 });
 
 module.exports = router;
