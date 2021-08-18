17d3f6dd32fc62bc71863eaa3d4a906220a81f90
gaiyadev <gaiyaobed94@gmail.com>
2021-08-18 18:54:39 +0100

    fixes

--- app.js
+++ app.js
@@ -21,4 +21,11 @@ app.use("/", indexRouter);
 app.use(`/api/${version}/users`, usersRouter);
 app.use(`/api/${version}/contact`, resumesRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("/client/dist/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
 module.exports = app;
