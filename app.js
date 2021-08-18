fe3a283f04f69523f7c88a331fe72ba10c47ba26
gaiyadev <gaiyaobed94@gmail.com>
2021-08-18 19:21:44 +0100

    fixj

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use(`/api/${version}/contact`, resumesRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("/client/next/server/pages/index.html"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  //   app.get("*", (req, res) => {
+  //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  //   });
 }
 
 module.exports = app;
