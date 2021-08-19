71cca31b1bfbc19b793e6006f458fbb7f509b098
gaiyadev <gaiyaobed94@gmail.com>
2021-08-19 01:03:35 +0100

    bugk

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("/client/server/pages/index.html"));
   app.get("/", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "client", "next/server/pages/", "index.html")
+      path.resolve(__dirname, "client", "/next/server/pages/", "index.html")
     );
   });
 }
